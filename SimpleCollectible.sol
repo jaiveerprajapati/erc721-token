39d2fac900c0b3b4c8fb8ecb4b783cf2409c1fc6
Jaiveer Prajapati <63976040+jaiveerprajapati@users.noreply.github.com>
2022-03-16 22:15:43 +0530

    Update SimpleCollectible.sol

--- SimpleCollectible.sol
+++ SimpleCollectible.sol
@@ -3,9 +3,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
     constructor () public ERC721 ("Dogie", "DOG") {
-        tokenCounter =0;
-    
-    }
+        tokenCounter = 0;
+        }
     function createCollectible(string memory tokenURI) public returns(uint256) {
        uint256 newItemId = tokenCounter;
         _safeMint(msg.sender, newItemId);
